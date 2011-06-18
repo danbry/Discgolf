705e9af3d7bd419576208f02414b09fa4d6e45dc
Dan Bryntze <danbryntze@Dan-Bryntzes-MacBook-Pro.local>
2011-06-18 08:34:47 +0200

    Initial Commit

---
+++ Discgolf/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Discgolf
+//
+//  Created by Dan Bryntze on 6/18/11.
+//  Copyright Omegapoint AB 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
